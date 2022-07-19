6460dd62fe6b47b1fd2709896c3c8ceb277f4e0b
jordanrings <jordanringsakabe@gmail.com>
2022-07-19 17:56:23 -0600

    got the second api call to work

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,25 @@
 var cityNameEl = $("#city-name");
 var apiKey = "c30976a5c3073479b6a8bf4fa197f32c";
-var cityInput = $("#input-text-field");     
+var cityInput = $("#input-text-field");
 var searchBtn = $("#search-btn");
+var latVal = $("#lat-val");
+var lonVal = $("#lon-val");
+var part = "";
 
 
-
-$("form").on("submit", function(event) {
+$("form").on("submit", function (event) {
     event.preventDefault();
     alert("test");
-    findLatandLon ()
-    updateCityName()
+    updateCityName();
+    findLatandLon();
+
 });
 
+var cityLat = "";
+var cityLon = "";
+
 function findLatandLon() {
-    var geoApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + $("#input-text-field").val() + '&limit=1&appid=' + apiKey;
+    var geoApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.val() + '&limit=1&appid=' + apiKey;
     fetch(geoApiUrl).then(function (response) {
         return response.json()
     }).then(function (data) {
@@ -21,11 +27,29 @@ function findLatandLon() {
         var cityLat = data[0].lat
         var cityLon = data[0].lon
         console.log(cityLat + " and " + cityLon)
-        
-    });
+        latVal.text(cityLat)
+        lonVal.text(cityLon)
+        var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=" + part + "&appid=" + apiKey;
+        fetch(weatherApiUrl).then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            console.log(data)
+            console.log("saved latval : " + latVal.val())
+        });
+    })
+};
 
-}
 
-function updateCityName(){
-    $("#city-name").text($("#input-text-field").val());
+// function findWeatherInfo() {
+//     var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=" + part + "&appid=" + apiKey;
+//     fetch(weatherApiUrl).then(function (response) {
+//         return response.json()
+//     }).then(function (data) {
+//         console.log(data)
+//         console.log("saved latval : " + latVal.val())
+//     });
+// }
+
+function updateCityName() {
+    $("#city-name").text($("#input-text-field").val().toUpperCase());
 }
