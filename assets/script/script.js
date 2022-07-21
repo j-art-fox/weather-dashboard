02adb6749f3ef536ba2ec524e7adf9f86cdd00b0
jordanrings <jordanringsakabe@gmail.com>
2022-07-21 14:56:00 -0600

    added cards for the next four days

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,21 +11,25 @@ var currWin = $('#curr-win');
 var currHum = $('#curr-hum');
 var currUV = $('#curr-uvindex');
 var weaIcon = $('#weather-icon');
+var weaIcon2 = $('#weather-icon-2');
+var weaIcon3 = $('#weather-icon-3');
+var weaIcon4 = $('#weather-icon-4');
+var weaIcon5 = $('#weather-icon-5');
 var part = "";
-// var usabledate = date()
+    // var usabledate = date()
 
-$("form").on("submit", function (event) {
-    event.preventDefault();
-    alert("test");
-    updateCityName();
-    findLatandLon();
+    $("form").on("submit", function (event) {
+        event.preventDefault();
+        alert("test");
+        updateCityName();
+        fetchWeather();
 
-});
+    });
 
 var cityLat = "";
 var cityLon = "";
 
-function findLatandLon() {
+function fetchWeather() {
     var geoApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.val() + '&limit=1&appid=' + apiKey;
     fetch(geoApiUrl).then(function (response) {
         return response.json()
@@ -42,6 +46,10 @@ function findLatandLon() {
         }).then(function (data) {
             console.log(data)
             var iconId = data.current.weather[0].icon
+            var iconId2 = data.daily[0].weather[0].icon
+            var iconId3 = data.daily[1].weather[0].icon
+            var iconId4 = data.daily[2].weather[0].icon
+            var iconId5 = data.daily[3].weather[0].icon
             currWea.text(data.current.weather[0].description)
             currWin.text(data.current.wind_speed + "mph")
             currHum.text(data.current.humidity)
@@ -50,12 +58,32 @@ function findLatandLon() {
             var currTimeUnix = data.current.dt
             var currDT = moment.unix(currTimeUnix).format("MMM Do, YYYY, hh:mm:ss")
             currDat.text(currDT)
-            weaIcon.attr("src","http://openweathermap.org/img/wn/"+iconId+"@2x.png")
+            weaIcon.attr("src", "http://openweathermap.org/img/wn/" + iconId + "@2x.png")
+            weaIcon2.attr("src", "http://openweathermap.org/img/wn/" + iconId2 + "@2x.png")
+            weaIcon3.attr("src", "http://openweathermap.org/img/wn/" + iconId3 + "@2x.png")
+            weaIcon4.attr("src", "http://openweathermap.org/img/wn/" + iconId4 + "@2x.png")
+            weaIcon5.attr("src", "http://openweathermap.org/img/wn/" + iconId5 + "@2x.png")
+            updateDates()
 
         });
     })
 };
 
+var tomorrow = moment().add(1,"days").format("MMM Do, YYYY");
+var theDayAfterTomorrow = moment().add(2,"days").format("MMM Do, YYYY");
+var theDayAfterTheDayAfterTomorrow = moment().add(3,"days").format("MMM Do, YYYY");
+var theDayAftertheDayAfterTheDayAfterTomorrow = moment().add(4,"days").format("MMM Do, YYYY");
+var day2 = $('#day-2');
+var day3 = $('#day-3');
+var day4 = $('#day-4');
+var day5 = $('#day-5');
+
+function updateDates(){
+    day2.text(tomorrow)
+    day3.text(theDayAfterTomorrow)
+    day4.text(theDayAfterTheDayAfterTomorrow)
+    day5.text(theDayAftertheDayAfterTheDayAfterTomorrow)
+}
 
 // function findWeatherInfo() {
 //     var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=" + part + "&appid=" + apiKey;
